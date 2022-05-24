2bfef8b7a6a8a99c5246801b7adf2d9cd740f8cf
renanlJ <renan_lemos@outlook.com>
2022-05-23 22:29:13 -0300

    preparando o jogo forca e manipulacao de strings

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,8 +1,25 @@
-
 def jogar():
     print('*********************************')
     print('***Bem vindo no jogo de forca!***')
     print('*********************************')
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index += 1
+
+        print("Jogando...")
 
     print("Fim do jogo")
 
