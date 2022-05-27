c8c0d304697c1221486ba54c0b793e926c0d720d
renanlJ <renan_lemos@outlook.com>
2022-05-26 23:27:10 -0300

    list, tuples e list comprehensions

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,23 +3,37 @@ def jogar():
     print('***Bem vindo no jogo de forca!***')
     print('*********************************')
 
-    palavra_secreta = "banana"
+    palavra_secreta = "pera".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print(f"Encontrei a letra {letra} na posição {index}")
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        print("Jogando...")
+        print(letras_acertadas)
+        enforcou = 6 == erros
+        acertou = "_" not in letras_acertadas
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu")
 
     print("Fim do jogo")
 
